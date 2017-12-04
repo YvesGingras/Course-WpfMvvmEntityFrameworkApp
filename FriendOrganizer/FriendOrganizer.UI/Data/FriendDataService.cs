aa054fe7cc028de8809dad03dcb700da27f0b073
Yves Gingras <yves.gingras@icloud.com>
2017-12-04 15:58:40 -0500

    Module 4; Demo 'Plan the User Interface Layer'.

---
+++ FriendOrganizer/FriendOrganizer.UI/Data/FriendDataService.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using FriendOrganizer.Model;
+
+namespace FriendOrganizer.UI.Data
+{
+    public class FriendDataService : IFriendDataService
+    {
+        public IEnumerable<Friend> GetAll() {
+            //Todo: Load data from real database.
+            yield return new Friend {FirstName = "Thomas", LastName = "Huber"};
+            yield return new Friend {FirstName = "Andreas", LastName = "Boelher"};
+            yield return new Friend {FirstName = "Julia", LastName = "Huber"};
+            yield return new Friend {FirstName = "Chrissi", LastName = "Egin"};
+        }
+    }
+}
+ 
