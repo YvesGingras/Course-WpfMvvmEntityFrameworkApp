f36c647d19ae29cbabe042209c0fe79d83519140
Yves Gingras <yves.gingras@icloud.com>
2017-12-09 17:49:28 -0500

    Module 6; Demo 'Create the LookupDataService'.

---
+++ FriendOrganizer/FriendOrganizer.UI/Data/IFriendLookupDataService.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using FriendOrganizer.Model;
+
+namespace FriendOrganizer.UI.Data {
+    public interface IFriendLookupDataService {
+        Task<IEnumerable<LookupItem>> GetFriendLookupAsync();
+    }
+}
