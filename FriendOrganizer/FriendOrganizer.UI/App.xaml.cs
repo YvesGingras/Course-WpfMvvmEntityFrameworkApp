d9361d764fe9d917bc1684668cbba378b95eeebd
Yves Gingras <yves.gingras@icloud.com>
2017-12-09 07:15:51 -0500

    Module 4; Demo 'Add Autofac for Dependency Injection'.

--- FriendOrganizer/FriendOrganizer.UI/App.xaml.cs
+++ FriendOrganizer/FriendOrganizer.UI/App.xaml.cs
@@ -1,19 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
-using FriendOrganizer.UI.Data;
-using FriendOrganizer.UI.ViewModel;
+﻿using System.Windows;
+using Autofac;
+using FriendOrganizer.UI.Startup;
 
 namespace FriendOrganizer.UI
 {
     public partial class App : Application
     {
         private void App_OnStartup(object sender, StartupEventArgs e) {
-            var mainWindow = new MainWindow(new MainViewModel(new FriendDataService()));
+            var bootstrapper = new Bootstrapper();
+            var constainer = bootstrapper.Bootstrap();
+
+            var mainWindow = constainer.Resolve<MainWindow>();
+            //var mainWindow = new MainWindow(new MainViewModel(new FriendDataService()));
+
             mainWindow.Show();
         }
     }
---
+++ FriendOrganizer/FriendOrganizer.UI/Startup/Bootstrapper.cs
@@ -0,0 +1,20 @@
+﻿using Autofac;
+using FriendOrganizer.UI.Data;
+using FriendOrganizer.UI.ViewModel;
+
+namespace FriendOrganizer.UI.Startup
+{
+    /// <summary>Class responsible to create the 'autofac container.</summary>
+    public class Bootstrapper
+    {
+        public IContainer Bootstrap() {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterType<MainWindow>().AsSelf();
+            builder.RegisterType<MainViewModel>().AsSelf();
+            builder.RegisterType<FriendDataService>().As<IFriendDataService>();
+
+            return builder.Build();
+        }
+    }
+}
