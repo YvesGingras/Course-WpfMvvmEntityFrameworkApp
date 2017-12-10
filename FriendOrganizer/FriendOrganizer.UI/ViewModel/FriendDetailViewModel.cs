dee0f9afd13b2c6f7665102948681a489245c66b
Yves Gingras <yves.gingras@icloud.com>
2017-12-10 18:13:47 -0500

    Module 6; Demo 'Subscribe to the OpenFriendDetailViewEnvent'.
    Module 6; Demo 'Subscribe to the OpenFriendDetailViewEvent.

--- FriendOrganizer/FriendOrganizer.UI/ViewModel/FriendDetailViewModel.cs
+++ FriendOrganizer/FriendOrganizer.UI/ViewModel/FriendDetailViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using System.Threading.Tasks;
 using FriendOrganizer.Model;
 using FriendOrganizer.UI.Data;
+using FriendOrganizer.UI.Event;
+using Prism.Events;
 
 namespace FriendOrganizer.UI.ViewModel
 {
@@ -8,9 +10,17 @@ namespace FriendOrganizer.UI.ViewModel
     {
         private readonly IFriendDataService _dataService;
         private Friend _friend;
+        private IEventAggregator _eventAggregator;
 
-        public FriendDetailViewModel(IFriendDataService dataService) {
+        public FriendDetailViewModel(IFriendDataService dataService, IEventAggregator evanAggregator) {
             _dataService = dataService;
+            _eventAggregator = evanAggregator;
+            _eventAggregator.GetEvent<OpenFriendDetailViewEvent>()
+                .Subscribe(OnOpenFriendDetailView);
+        }
+
+        private async void OnOpenFriendDetailView(int friendId) {
+            await LoadAsync(friendId);
         }
 
         public Friend Friend {
