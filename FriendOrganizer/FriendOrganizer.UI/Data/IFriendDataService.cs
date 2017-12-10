9721ef1ced6d7f72f08b3f3f379c3709cbbebe27
Yves Gingras <yves.gingras@icloud.com>
2017-12-10 15:47:43 -0500

    Module 6; Demo 'Adjust the FriendDataService

--- FriendOrganizer/FriendOrganizer.UI/Data/IFriendDataService.cs
+++ FriendOrganizer/FriendOrganizer.UI/Data/IFriendDataService.cs
@@ -4,6 +4,6 @@ using FriendOrganizer.Model;
 
 namespace FriendOrganizer.UI.Data {
     public interface IFriendDataService {
-        Task<List<Friend>> GetAllAsync();
+        Task<Friend> GetByIdAsync(int friendId);
     }
 }           
