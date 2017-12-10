0df0b5e9f63f1926e4d61fbe8d7e2ab4376d6a91
Yves Gingras <yves.gingras@icloud.com>
2017-12-10 18:00:18 -0500

    Module 6; Demo 'Publish the OpenFriednDetailViewEvent'.

--- FriendOrganizer/FriendOrganizer.UI/ViewModel/NavigationViewModel.cs
+++ FriendOrganizer/FriendOrganizer.UI/ViewModel/NavigationViewModel.cs
@@ -2,31 +2,41 @@
 using System.Threading.Tasks;
 using FriendOrganizer.Model;
 using FriendOrganizer.UI.Data;
+using FriendOrganizer.UI.Event;
+using Prism.Events;
 
 namespace FriendOrganizer.UI.ViewModel
 {
-    public class NavigationViewModel : INavigationViewModel
+    public class NavigationViewModel : ViewModelBase, INavigationViewModel
     {
         private IFriendLookupDataService _friendLookupService;
+        private IEventAggregator _eventAggragator;
+        private LookupItem _selectedFriend;
 
-        public NavigationViewModel(IFriendLookupDataService friendLookupService) {
+        public NavigationViewModel(IFriendLookupDataService friendLookupService, IEventAggregator eventAggregator) {
             _friendLookupService = friendLookupService;
+            _eventAggragator = eventAggregator;
             Friends = new ObservableCollection<LookupItem>();
         }
 
         public ObservableCollection<LookupItem> Friends { get; set; }
-        
-        public async Task LoadAsync() {
-            var lookupItems = await _friendLookupService.GetFriendLookupAsync();
-            Friends.Clear();
-            foreach (var lookupItem in lookupItems) {
-                Friends.Add(lookupItem);
+
+        /*Used by NavigationView.xml*/
+        public LookupItem SelectedFriend {
+            get => _selectedFriend;
+            set {
+                _selectedFriend = value;
+                OnPropertyChanged();
+                if (_selectedFriend != null)
+                    _eventAggragator.GetEvent<OpenFriendDetailViewEvent>()
+                        .Publish(_selectedFriend.Id);
             }
         }
 
-
+        public async Task LoadAsync() {
+            var lookupItems = await _friendLookupService.GetFriendLookupAsync();
+            Friends.Clear();
+            foreach (var lookupItem in lookupItems) Friends.Add(lookupItem);
+        }
     }
-
-
-    
 }
