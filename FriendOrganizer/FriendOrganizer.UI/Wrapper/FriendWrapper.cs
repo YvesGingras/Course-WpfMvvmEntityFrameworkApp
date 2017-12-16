2b488bcdc658a05f2f3a69b7051d4e97eadfa484
Yves Gingras <yves.gingras@icloud.com>
2017-12-16 10:19:19 -0500

    Module 9; Demo 'Implement INotifyDataErrorInfo'.

--- FriendOrganizer/FriendOrganizer.UI/Wrapper/FriendWrapper.cs
+++ FriendOrganizer/FriendOrganizer.UI/Wrapper/FriendWrapper.cs
@@ -1,10 +1,17 @@
-﻿using FriendOrganizer.Model;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using FriendOrganizer.Model;
 using FriendOrganizer.UI.ViewModel;
 
 namespace FriendOrganizer.UI.Wrapper
 {
-    public class FriendWrapper:ViewModelBase
+    public class FriendWrapper:ViewModelBase,INotifyDataErrorInfo
     {
+        private Dictionary<string, List<string>> _errorsByPropertyName = new Dictionary<string, List<string>>();
+
         public FriendWrapper(Friend model) {
             Model = model;
         }
@@ -35,6 +42,37 @@ namespace FriendOrganizer.UI.Wrapper
                 Model.Email = value;
                 OnPropertyChanged();
             } 
+        }
+
+        public bool HasErrors => _errorsByPropertyName.Any();
+
+        public event EventHandler<DataErrorsChangedEventArgs> ErrorsChanged;
+
+        public IEnumerable GetErrors(string propertyName) {
+            return _errorsByPropertyName.ContainsKey(propertyName)
+                ? _errorsByPropertyName[propertyName]
+                : null;
+        }
+        
+        private void OnErrorsChanged(string propertyName) {
+            ErrorsChanged?.Invoke(this,new DataErrorsChangedEventArgs(propertyName));
+        }
+
+        /// <summary>Simplify the way to add an error to the 'errors Dictionary'."/>.</summary>
+        private void AddError(string propertyName, string error) {
+            if(!_errorsByPropertyName.ContainsKey(propertyName))
+                _errorsByPropertyName[propertyName] = new List<string>();
+
+            if (!_errorsByPropertyName[propertyName].Contains(error)) {
+                _errorsByPropertyName[propertyName].Add(error);
+                OnErrorsChanged(propertyName);
+            }
+        }
+
+        /// <summary>Simplify the way to remove errors from the 'errors Dictionary'."/>.</summary>
+        private void ClearErrors(string propertyName) {
+            _errorsByPropertyName.Remove(propertyName);
+            OnErrorsChanged(propertyName);
         }
     }
 }
