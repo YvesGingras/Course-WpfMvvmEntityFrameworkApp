cb1320ff0f35677c90c71d73337e099775d137e6
Yves Gingras <yves.gingras@icloud.com>
2017-12-16 16:29:42 -0500

    Module 9; Move NotifyDataErrorInfoBase Class to a separate folder'.

--- FriendOrganizer/FriendOrganizer.UI/Wrapper/FriendWrapper.cs
+++ FriendOrganizer/FriendOrganizer.UI/Wrapper/FriendWrapper.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
 using System.Runtime.Serialization;
 using FriendOrganizer.Model;
-using FriendOrganizer.UI.ViewModel;
 
 namespace FriendOrganizer.UI.Wrapper
 {
@@ -53,47 +48,6 @@ namespace FriendOrganizer.UI.Wrapper
                 Model.Email = value;
                 OnPropertyChanged();
             } 
-        }
-    }
-
-    public class NotifyDataErrorBase:ViewModelBase,INotifyDataErrorInfo
-    {
-        private Dictionary<string, List<string>> _errorsByPropertyName = new Dictionary<string, List<string>>();
-
-        public bool HasErrors => _errorsByPropertyName.Any();
-
-        public event EventHandler<DataErrorsChangedEventArgs> ErrorsChanged;
-
-        public IEnumerable GetErrors(string propertyName)
-        {
-            return _errorsByPropertyName.ContainsKey(propertyName)
-                ? _errorsByPropertyName[propertyName]
-                : null;
-        }
-
-        protected virtual void OnErrorsChanged(string propertyName)
-        {
-            ErrorsChanged?.Invoke(this, new DataErrorsChangedEventArgs(propertyName));
-        }
-
-        /// <summary>Simplify the way to add an error to the 'errors Dictionary'."/>.</summary>
-        protected void AddError(string propertyName, string error)
-        {
-            if (!_errorsByPropertyName.ContainsKey(propertyName))
-                _errorsByPropertyName[propertyName] = new List<string>();
-
-            if (!_errorsByPropertyName[propertyName].Contains(error))
-            {
-                _errorsByPropertyName[propertyName].Add(error);
-                OnErrorsChanged(propertyName);
-            }
-        }
-
-        /// <summary>Simplify the way to remove errors from the 'errors Dictionary'."/>.</summary>
-        protected void ClearErrors(string propertyName)
-        {
-            _errorsByPropertyName.Remove(propertyName);
-            OnErrorsChanged(propertyName);
         }
     }
 }
