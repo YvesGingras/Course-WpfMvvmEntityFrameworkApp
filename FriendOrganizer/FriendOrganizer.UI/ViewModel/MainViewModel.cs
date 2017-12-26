06882a32f4ba2b10239a010ce3fa42c1b2a1b5df
Yves Gingras <yves.gingras@icloud.com>
2017-12-26 01:22:57 -0500

    Module 10; Demo 'Create a MessageDialogService'.

--- FriendOrganizer/FriendOrganizer.UI/ViewModel/MainViewModel.cs
+++ FriendOrganizer/FriendOrganizer.UI/ViewModel/MainViewModel.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Threading.Tasks;
-using System.Windows;
 using FriendOrganizer.UI.Event;
+using FriendOrganizer.UI.View.Services;
 using Prism.Events;
 
 namespace FriendOrganizer.UI.ViewModel
@@ -11,13 +11,15 @@ namespace FriendOrganizer.UI.ViewModel
         private readonly IEventAggregator _eventAggregator;
         private readonly Func<IFriendDetailViewModel> _friendDetailViewModelCreator;
         private IFriendDetailViewModel _friendDetailViewModel;
+        private readonly IMessageDialogService _messageDialogService;
 
         public MainViewModel(INavigationViewModel navigationViewModel,
             Func<IFriendDetailViewModel> friendDetailViewModelCreator,
-            IEventAggregator eventAggregator) {
+            IEventAggregator eventAggregator, IMessageDialogService messageDialogService) {
             _eventAggregator = eventAggregator;
 
             _friendDetailViewModelCreator = friendDetailViewModelCreator;
+            _messageDialogService = messageDialogService;
 
             _eventAggregator.GetEvent<OpenFriendDetailViewEvent>()
                 .Subscribe(OnOpenFriendDetailView);
@@ -42,9 +44,9 @@ namespace FriendOrganizer.UI.ViewModel
 
         private async void OnOpenFriendDetailView(int friendId) {
             if (FriendDetailViewModel != null && FriendDetailViewModel.HasChanges) {
-                var result = MessageBox.Show("You've made changes. Navigate away?", "Question",
-                    MessageBoxButton.OKCancel);
-                if (result == MessageBoxResult.Cancel)
+                var result = _messageDialogService.ShowOkCancelDialog("You've made changes. Navigate away?", "Question");
+
+                if (result == MessageDialogResult.Cancel)
                     return;
             }
             
