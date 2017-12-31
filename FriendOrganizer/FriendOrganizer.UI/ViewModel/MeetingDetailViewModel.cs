dc6971bd53162470d23980c8160dd069cbb1a540
Yves Gingras <yves.gingras@icloud.com>
2017-12-31 16:50:22 -0500

    Module 15; Demo 'Create the MeetingDetailViewModel'.

---
+++ FriendOrganizer/FriendOrganizer.UI/ViewModel/MeetingDetailViewModel.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Threading.Tasks;
+using FriendOrganizer.Model;
+using FriendOrganizer.UI.Data.Repositories;
+using FriendOrganizer.UI.View.Services;
+using FriendOrganizer.UI.Wrapper;
+using Prism.Commands;
+using Prism.Events;
+
+namespace FriendOrganizer.UI.ViewModel
+{
+    public class MeetingDetailViewModel : DetailViewModelBase, IMeetingDetailViewModel
+    {
+        private readonly IMessageDialogService _messageDialogService;
+        private readonly IMeetingRepository _meetingRepository;
+        private MeetingWrapper _meetingWrapper;
+
+        public MeetingDetailViewModel(IEventAggregator eventAggregator,
+            IMessageDialogService messageDialogService,
+            IMeetingRepository meetingRepository) : base(eventAggregator) {
+            _messageDialogService = messageDialogService;
+            _meetingRepository = meetingRepository;
+        }
+
+        public MeetingWrapper Meeting {
+            get => _meetingWrapper;
+            set {
+                _meetingWrapper = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public override async Task LoadAsync(int? meetingId) {
+            var meeting = meetingId.HasValue
+                ? await _meetingRepository.GetByIdAsync(meetingId.Value)
+                : CreateNewMeeting();
+
+            InitializeMeeting(meeting);
+        }
+
+        private Meeting CreateNewMeeting()
+        {
+            var meeting = new Meeting
+            {
+                DateFrom = DateTime.Now.Date,
+                DateTo = DateTime.Now.Date
+            };
+
+            _meetingRepository.Add(meeting);
+            return meeting;
+        }
+
+        private void InitializeMeeting(Meeting meeting) {
+            Meeting = new MeetingWrapper(meeting);
+
+            Meeting.PropertyChanged += (s, e) => {
+                if (!HasChanges)
+                    HasChanges = _meetingRepository.HasChanges();
+
+                if(e.PropertyName == nameof(Meeting.HasErrors))
+                    ((DelegateCommand) SaveCommand).RaiseCanExecuteChanged();
+            };
+            ((DelegateCommand)SaveCommand).RaiseCanExecuteChanged();
+        }
+
+        protected override void OnDeleteExecute() {
+            var result = _messageDialogService.ShowOkCancelDialog($"Do you really want to delete the meeting {Meeting.Title}?", "Question");
+            if (result == MessageDialogResult.Ok) {
+                _meetingRepository.Remove(Meeting.Model);
+                _meetingRepository.SaveAsync();
+                RaiseDetailDeletedEvent(Meeting.Id);
+            }
+        }
+
+        protected override bool OnSaveCanExecute() {
+            return Meeting != null && !Meeting.HasErrors && HasChanges;
+        }
+
+        protected override void OnSaveExecute() {
+            _meetingRepository.SaveAsync();
+            HasChanges = _meetingRepository.HasChanges();
+            RaiseDetailSavedEvent(Meeting.Id,Meeting.Title);
+        }
+    }
+}
